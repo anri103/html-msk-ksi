34049acdfa9b15950529fec0230b6ebbe3c0e415
anri103 <ianrianri@gmail.com>
2025-05-26 00:31:49 +0300

    -

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
-
+// swiper
+const swiperProjects = new Swiper('.swiperProjects', {
+  slidesPerView: 1,
+  spaceBetween: 16,
+  loop: false,
+  grabCursor: true,
+  autoplay: {
+    delay: 5000,
+    // disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  breakpoints: {
+    992: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+    1200: {
+      slidesPerView: 2,
+      spaceBetween: 24,
+    },
+  },
+});
 
 // Fancybox
 Fancybox.bind("[data-fancybox]", {
